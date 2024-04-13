791fc64916c70d1b6debb2c8afcc80ed19dddd49
Ewi2 <renegonzalezb9@gmail.com>
2024-04-13 09:09:55 -0600

    first commit

---
+++ laboratorio_2_computo_2/resources/views/acerca.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <title>Acerca</title>
+</head>
+<body>
+    <h1 class="T5">Integrantes:</h1>
+    <h3 class="T5">Diego Alejandro Rodríguez Valencia SMSS044522</h3>
+    <h3 class="T5">Francisco Javier Lemus Cruz SMSS063722</h3>
+    <h3 class="T5">Rony Josué Sorto Villalta SMSS021622</h3>
+    <h3 class="T5">Edwin René González Bonilla SMSSS086022</h3>
+</body>
+</html>
---
+++ laboratorio_2_computo_2/resources/views/home.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <title>Inicio</title>
+</head>
+<body>
+<header>
+        <div class="inner">
+            <div class="logo">
+                <div>
+                    <h1 class="home_headerTitle">Laboratorio 2</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <br>
+    <br>
+    <h1 class="home_titulos">Nuestro proyecto:</h1>
+    <br>
+    <br>
+    <p class="paragraph_info">¡Bienvenido/a a nuestra página dedicada al tema del consumo excesivo de videojuegos!</p>
+    <br>
+    <p class="paragraph_info">En la era digital en la que vivimos, los videojuegos se 
+        han convertido en una forma popular de entretenimiento y 
+        escapismo para personas de todas las edades. Sin embargo, 
+        detrás de la diversión y la emoción que ofrecen, existe una 
+        preocupación creciente sobre el impacto que el consumo 
+        excesivo de videojuegos puede tener en la salud física, 
+        mental y emocional de los jugadores.</p>
+    <br>
+    <p class="paragraph_info">En esta página, exploraremos en profundidad los diversos 
+        aspectos relacionados con el consumo excesivo de videojuegos. 
+        Desde los efectos negativos potenciales en el bienestar personal 
+        y las relaciones interpersonales, hasta las estrategias para 
+        establecer un equilibrio saludable entre el tiempo dedicado a 
+        los videojuegos y otras actividades importantes en la vida diaria.</p>
+    <br>
+    <p class="paragraph_info">Nuestro objetivo es proporcionar información objetiva, 
+        consejos prácticos y recursos útiles para aquellos que 
+        buscan comprender mejor este tema y tomar decisiones informadas 
+        sobre su propia relación con los videojuegos.</p>
+    <br>
+    <p class="paragraph_info">Te invitamos a explorar los diferentes apartados 
+        de nuestra página y unirte a la conversación sobre este 
+        tema tan relevante en la sociedad actual. Juntos, podemos 
+        promover un uso responsable y consciente de los videojuegos para 
+        garantizar que sigan siendo una fuente de diversión y disfrute 
+        sin comprometer nuestro bienestar general.</p>
+    <br>
+    <p class="paragraph_info">¡Gracias por visitarnos y esperamos que encuentres esta página informativa y útil!</p>
+
+    <br>
+    <br>
+    <br>
+
+    <p class="paragraph_title">¿Cuál es el propósito específico de este proyecto?</p>
+    <p class="paragraph_info">Es abordar el tema del consumo excesivo 
+        de videojuegos en estudiantes mediante la creación 
+        de conciencia y el ofrecimiento de soluciones prácticas para 
+        promover un equilibrio saludable entre el tiempo dedicado 
+        a los videojuegos y otras actividades importantes, como el estudio, 
+        la socialización y el ejercicio.</p>
+    <br>
+    <p class="paragraph_info"> Como idea principal podemos desarrollar una plataforma interactiva 
+        que combine información educativa con herramientas prácticas para 
+        promover un equilibrio saludable en el uso de los videojuegos.</p>
+    <br>
+    <br>
+    <p class="paragraph_title">¿Está dirigido a los propios estudiantes, 
+        a padres preocupados, a educadores o a profesionales de la salud mental?</p>
+    <br>
+    <p class="paragraph_info">Nuestro público objetivo abarca a estudiantes, 
+        padres, educadores y profesionales de la salud mental interesados 
+        en entender y abordar el problema del consumo excesivo de 
+        videojuegos desde una perspectiva integral.</p>
+    <br>
+    <p class="paragraph_info">Tomando en cuenta todo nuestro público, 
+        nuestro plan es crear una sección en el sitio web dedicada a 
+        recursos específicos para padres, como guías de conversación 
+        y estrategias para fomentar un uso saludable de los videojuegos en el hogar.</p>
+    <br>
+    <br>
+    <p class="paragraph_title">¿Cuál será la estrategia de difusión para 
+        llegar al público objetivo y generar impacto con este proyecto?</p>
+    <br>
+    <p class="paragraph_info">Nuestra estrategia de difusión incluirá colaboraciones 
+        con instituciones educativas, organizaciones juveniles y medios de comunicación 
+        para llegar de manera efectiva a nuestro público objetivo y generar conciencia sobre el tema.</p>
+    <br>
+    <p class="paragraph_info">Una de nuestras ideas es elmplementar herramientas de análisis 
+        web para rastrear métricas clave, como el tiempo promedio en el sitio, 
+        la tasa de participación en encuestas y el número de usuarios que implementan 
+        cambios positivos en su comportamiento.</p>
+</body>
+</html>
